988ac3ebf91f0090a580448b99297137cdb6f08b
Simonrak <72688649+Simonrak@users.noreply.github.com>
2024-09-08 15:34:20 +0200

    Update setup_full.sh

--- setup_full.sh
+++ setup_full.sh
@@ -1,40 +1,42 @@
 #!/bin/sh
 
 elevate_privileges() {
-    sudo -v > /dev/null 2>&1
-    while true; do sudo -v > /dev/null 2>&1; sleep 60; done &
+    sudo -v
+    while true; do sudo -v; sleep 60; done &
     SUDO_PID=$!
 }
 
+install_libssl() {
+    echo "Installing libssl1.1..."
+    cd /tmp
+    wget https://archive.archlinux.org/packages/o/openssl-1.1/openssl-1.1-1.1.1.u-1-x86_64.pkg.tar.zst
+    sudo pacman -U --noconfirm openssl-1.1-1.1.1.u-1-x86_64.pkg.tar.zst
+    cd -
+    echo "libssl1.1 installed successfully."
+}
+
 install_paru() {
-    if ! command -v paru ; then
-        echo "Installing paru..."
-        sleep 1
-        sudo pacman -S --needed base-devel git wget > /dev/null 2>&1
-        git clone https://aur.archlinux.org/paru.git > /dev/null 2>&1
-        pushd paru > /dev/null
-        makepkg -si --noconfirm > /dev/null 2>&1
-        popd > /dev/null
-        rm -rf paru
-        echo "Paru installed successfully."
-        sleep 1
-    else
-        echo "Paru is already installed, skipping installation."
-        sleep 1
-    fi
+    sudo pacman -S --needed base-devel git wget make libssl1.1
+    echo "Installing paru..."
+    cd /tmp
+    wget https://github.com/Morganamilo/paru/releases/download/v2.0.3/paru-v2.0.3-x86_64.tar.zst
+    tar -xvf paru-v2.0.3-x86_64.tar.zst
+    sudo mv paru /usr/local/bin/paru
+    cd -
+    echo "paru installed successfully."
 }
 
 install_packages() {
-    PACKAGES="aptitude curl wget pipx python3-dev zsh zoxide nano-syntax-highlighting zsh-autosuggestions zsh-syntax-highlighting unzip python python-pip tmux fzf cronie eza"
+    PACKAGES="curl wget python python-pip zsh tmux fzf cronie unzip pipx zoxide nano-syntax-highlighting zsh-autosuggestions zsh-syntax-highlighting eza"
+
     echo "Installing packages..."
     sleep 1
     for pkg in $PACKAGES; do
-        if ! paru -Q $pkg ; then
-            if ! paru -S --noconfirm $pkg > /dev/null 2>&1; then
-                echo "Failed to install $pkg. Skipping."
-            fi
+        if ! paru -S --noconfirm $pkg; then
+            echo "Failed to install $pkg. Skipping."
         fi
     done
+
     echo "Package installation complete."
     echo "Zsh with plugins"
     echo "Zoxide (run 'z' followed by a previously entered directory (fuzzy))"
@@ -45,19 +47,19 @@ install_packages() {
 }
 
 install_thefuck() {
-    if ! pacman -Qi thefuck ; then
-        sudo pacman -S --noconfirm thefuck > /dev/null 2>&1
+    if ! pacman -Qi thefuck; then
+        sudo pacman -S --noconfirm thefuck
     fi
 
-    if ! python3 -m pip show thefuck ; then
-        python3 -m pip install thefuck > /dev/null 2>&1
+    if ! python3 -m pip show thefuck; then
+        python3 -m pip install thefuck
     fi
 }
 
 configure_nano() {
     echo "Configuring nano..."
     sleep 1
-    curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh > /dev/null 2>&1
+    curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
     echo -e "set softwrap\nset boldtext\nset backup\nset autoindent\nset atblanks\nset mouse\nset tabsize 4\nset tabstospaces\nset linenumbers" >> ~/.nanorc
     echo "Nano configuration complete."
     echo "Added line numbers, syntax highlights and others."
@@ -67,10 +69,10 @@ configure_nano() {
 update_system_configs() {
     echo "Updating system configurations..."
     sleep 1
-    sudo sh -c 'echo -e "PermitRootLogin no\nUsePAM no\nAllowAgentForwarding yes\nClientAliveInterval 10\nClientAliveCountMax 0\nX11Forwarding yes\nX11DisplayOffset 10\nPrintMotd yes" >> /etc/ssh/sshd_config' > /dev/null 2>&1
-    sudo sh -c "echo '$USER    ALL=(ALL:ALL) ALL' >> /etc/sudoers" > /dev/null 2>&1
-    sudo sh -c 'echo -e "[interop]\nappendWindowsPath = false\n\n[boot]\nsystemd=true" >> /etc/wsl.conf' > /dev/null 2>&1
-    sudo sed -i 's/Prompt=lts/Prompt=latest/g' /etc/update-manager/release-upgrades > /dev/null 2>&1
+    sudo sh -c 'echo -e "PermitRootLogin no\nUsePAM no\nAllowAgentForwarding yes\nClientAliveInterval 10\nClientAliveCountMax 0\nX11Forwarding yes\nX11DisplayOffset 10\nPrintMotd yes" >> /etc/ssh/sshd_config'
+    sudo sh -c "echo '$USER    ALL=(ALL:ALL) ALL' >> /etc/sudoers"
+    sudo sh -c 'echo -e "[interop]\nappendWindowsPath = false\n\n[boot]\nsystemd=true" >> /etc/wsl.conf'
+    sudo sed -i 's/Prompt=lts/Prompt=latest/g' /etc/update-manager/release-upgrades
     echo "System configurations updated."
     sleep 1
 }
@@ -78,17 +80,28 @@ update_system_configs() {
 setup_zsh() {
     echo "Setting up zsh..."
     sleep 1
-    if [ ! -d "$HOME/.oh-my-zsh" ]; then
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null 2>&1
+    
+    if [ -d "$HOME/.oh-my-zsh" ]; then
+        rm -rf "$HOME/.oh-my-zsh"
     fi
-    echo "installing zsh theme and plugins"
+    
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    
+    echo "Installing zsh theme and plugins"
     sleep 1
-    ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
-    [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting > /dev/null 2>&1
-    [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions > /dev/null 2>&1
-    [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k > /dev/null 2>&1
-    [ ! -d "$HOME/.fzf" ] && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf > /dev/null 2>&1 && ~/.fzf/install --all > /dev/null 2>&1
-
+    
+    ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+    
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+    
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM}/themes/powerlevel10k
+    
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
+    
+    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+    sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
+    
     echo "Zsh setup complete."
     sleep 1
 }
@@ -96,7 +109,7 @@ setup_zsh() {
 configure_pip() {
     PIP_CONF=~/.config/pip/pip.conf
     if [ ! -f "$PIP_CONF" ]; then
-        mkdir -p ~/.config/pip > /dev/null 2>&1
+        mkdir -p ~/.config/pip
         echo -e "[global]\nbreak-system-packages=true" > "$PIP_CONF"
     fi
 }
@@ -104,11 +117,11 @@ configure_pip() {
 finalize_setup() {
     echo "Finalizing setup..."
     sleep 1
-    wget https://raw.githubusercontent.com/Simonrak/Archlinux-tools/main/z.zshrc -O ~/.zshrc > /dev/null 2>&1
-    sudo cp -r /usr/share/zsh/plugins /home/$USER/.oh-my-zsh/custom/ > /dev/null 2>&1
+    wget https://raw.githubusercontent.com/Simonrak/Archlinux-tools/main/z.zshrc -O ~/.zshrc
+    sudo cp -r /usr/share/zsh/plugins /home/$USER/.oh-my-zsh/custom/
     echo "Changing shell to zsh..."
     sleep 1
-    sudo chsh -s $(which zsh) $USER > /dev/null 2>&1
+    sudo chsh -s $(which zsh) $USER
     echo "Setup finalized."
     sleep 1
 }
@@ -117,15 +130,17 @@ main() {
     echo "Starting setup process..."
     sleep 1
     elevate_privileges
+    install_libssl
     install_paru
     install_packages
     install_thefuck
     update_system_configs
+    configure_nano
     setup_zsh
     configure_pip
     finalize_setup
 
-    kill $SUDO_PID > /dev/null 2>&1
+    kill $SUDO_PID
     sleep 1
     echo "Setup complete. Please log in again to apply all changes."
     sleep 3
